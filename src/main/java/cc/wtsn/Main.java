3cce50042c20beb8fa51de1330f623591abd38e6
JwAcre <git@jwacre.com>
2021-03-02 20:04:21 +0000

    Initial commit

---
+++ src/main/java/cc/wtsn/Main.java
@@ -0,0 +1,10 @@
+package cc.wtsn;
+
+import cc.wtsn.triton.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
